7efb91b0e19aae612fa459811b5f451530cbdd94
neev003 <92441656+neev003@users.noreply.github.com>
2024-03-08 23:41:22 +0530

    Create sumn.cpp

---
+++ sumn.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, from Isham\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Whatever\n";
+  return 0;
+}
